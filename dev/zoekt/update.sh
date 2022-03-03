bb75cc056c26d480b65856b76204882d7ce839fe
Dave Try <davetry@gmail.com>
2022-03-02 20:04:05 -0500

    does this run

--- dev/zoekt/update.sh
+++ dev/zoekt/update.sh
@@ -8,23 +8,23 @@ export GO111MODULE=on
 export GOPROXY="${GOPROXY:-direct}"
 
 # Can specify a SHA pushed to our fork instead of master
-version="${1:-master}"
+# version="${1:-master}"
 
-upstream=github.com/google/zoekt
-fork=github.com/sourcegraph/zoekt
+# upstream=github.com/google/zoekt
+# fork=github.com/sourcegraph/zoekt
 
-oldsha="$(go mod edit -print | grep "$fork" | grep -o '[a-f0-9]*$')"
-module="$(go get "${fork}@${version}" | grep -E -o ${fork}'@v0.0.0-[0-9a-z-]+')"
-newsha="$(echo "$module" | grep -o '[a-f0-9]*$')"
+# oldsha="$(go mod edit -print | grep "$fork" | grep -o '[a-f0-9]*$')"
+# module="$(go get "${fork}@${version}" | grep -E -o ${fork}'@v0.0.0-[0-9a-z-]+')"
+# newsha="$(echo "$module" | grep -o '[a-f0-9]*$')"
 
-echo "https://github.com/sourcegraph/zoekt/compare/$oldsha...$newsha"
-echo "git log --pretty=format:'- https://github.com/sourcegraph/zoekt/commit/%h %s' $oldsha...$newsha | sed 's/ (#[0-9]*)//g'"
-echo "git log --pretty=format:'- %h %s' $oldsha...$newsha | sed 's/ (#[0-9]*)//g'"
+# echo "https://github.com/sourcegraph/zoekt/compare/$oldsha...$newsha"
+# echo "git log --pretty=format:'- https://github.com/sourcegraph/zoekt/commit/%h %s' $oldsha...$newsha | sed 's/ (#[0-9]*)//g'"
+# echo "git log --pretty=format:'- %h %s' $oldsha...$newsha | sed 's/ (#[0-9]*)//g'"
 
-go mod edit "-replace=${upstream}=${module}"
-go mod download ${upstream}
-go mod tidy
+# go mod edit "-replace=${upstream}=${module}"
+# go mod download ${upstream}
+# go mod tidy
 
-echo "Ensure we update go.sum by actually compiling some code which depends on zoekt."
-echo "We do this by running 'go test' without actually running any tests."
-go test -run '^$' github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend
+# echo "Ensure we update go.sum by actually compiling some code which depends on zoekt."
+# echo "We do this by running 'go test' without actually running any tests."
+# go test -run '^$' github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend
